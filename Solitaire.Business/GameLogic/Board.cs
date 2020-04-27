fc5b160764f2df7cc3173b51f858f88e4d977daa
cristianagrigoriu <cristiana.grigoriu@gmail.com>
2020-04-27 08:01:31 +0300

    Add sketch for possible moves, board factory

--- Solitaire.Business/GameLogic/Board.cs
+++ Solitaire.Business/GameLogic/Board.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Solitaire.Business.Test")]
 namespace Solitaire.Business
@@ -8,5 +9,23 @@ namespace Solitaire.Business
         public FoundationPile[] FoundationPiles { get; set; }
         public TableauPile[] TableauPiles { get; set; }
         public Stock Stock { get; set; }
+
+        public void RevealTopCardFromStock()
+        {
+
+        }
+
+        public void MoveSelectedCardToFoundationPile(Card selectedCard)
+        {
+
+        }
+
+        public void MoveFromTableauPileToAnotherTableauPile(
+            List<Card> selectedCards, 
+            TableauPile sourcePile, 
+            TableauPile destinationPile)
+        {
+
+        }
     }
 }
---
+++ Solitaire.Business/GameLogic/BoardFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Solitaire.Business.GameLogic
+{
+    internal static class BoardFactory
+    {
+        public static Board GetSetupBoard()
+        {
+            return new BoardBuilder()
+                .WithPackOfCards()
+                .WithFoundationPiles()
+                .WithTableauPiles()
+                .WithStock()
+                .Build();
+        }
+    }
+}
